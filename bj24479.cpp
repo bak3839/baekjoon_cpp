6ec9746db1394d2f2695688e7a7360154a8e6e6f
bak3839 <101046875+bak3839@users.noreply.github.com>
2022-10-29 17:56:32 +0900

    백준 24479 알고리즘 수업 - 깊이 우선 탐색

---
+++ bj24479.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+vector<vector<int>> v(100001);
+vector<int> res(100001, 0);
+bool visited[100001];
+int cnt = 1;
+
+void dfs(int s) {
+	res[s] = cnt++;
+	visited[s] = true;
+
+	for (int i = 0;i < v[s].size();i++) {
+		if (!visited[v[s][i]]) dfs(v[s][i]);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, M, R, n, r;
+	cin >> N >> M >> R;
+
+	for (int i = 1;i <= M;i++) {
+		cin >> r >> n;
+		v[r].push_back(n);
+		v[n].push_back(r);
+	}
+
+	for (int i = 1;i <= N;i++) {
+		sort(v[i].begin(), v[i].end());
+	}
+
+	dfs(R);
+
+	for (int i = 1;i <= N;i++) cout << res[i] << "\n";
+
+	return 0;
+}
