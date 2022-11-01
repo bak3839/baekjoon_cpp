63bb494e2727024be33f12f493c3655a6ea3652a
bak3839 <101046875+bak3839@users.noreply.github.com>
2022-11-01 20:03:24 +0900

    백준 16435 스네이크버드

---
+++ bj16435.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, L;
+	cin >> N >> L;
+	vector<int> v(N);
+
+	for (int i = 0; i < N; i++) cin >> v[i];
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < N; i++) {
+		if (L >= v[i]) ++L;
+		else break;
+	}
+
+	cout << L;
+	return 0;
+}
