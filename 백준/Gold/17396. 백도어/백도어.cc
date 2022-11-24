921090acba624f79a6e3cfabe918aad277b50b18
bak3839 <101046875+bak3839@users.noreply.github.com>
2022-11-24 17:41:13 +0900

    [Gold V] Title: 백도어, Time: 132 ms, Memory: 16084 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.cc
+++ 백준/Gold/17396. 백도어/백도어.cc
@@ -24,13 +24,14 @@ void dijkstra(int s) {
         
         if (d[now] < dist && now == N - 1) break;
 
-        if (d[now] < dist) continue;
+        if (d[now] < dist) continue; // 이미 최단 경로를 찾은 정점이면 통과
 
         for (int i = 0; i < graph[now].size(); i++) {
-            int next = graph[now][i].first;
-            ll cost = dist + graph[now][i].second;
+            int next = graph[now][i].first;       // 다음 정점번호
+            ll cost = dist + graph[now][i].second; // 다음 정점까지 가는 비용
             
-            if (cost < d[next] && (!ward[next] || next == N - 1)) { 
+            // 다음 정점으로 가는 비용중 최단 거리 + 적시야가 없거나 넥서스일때
+            if (cost < d[next]) { 
                 pq.push({ -cost, next });
                 d[next] = cost;
             }
@@ -46,13 +47,15 @@ int main() {
     int s, e, t;
     cin >> N >> M;   
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) { // 적 시야
         cin >> t;
         ward.push_back(t);
     }
 
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < M; i++) { // 양방향 연결
         cin >> s >> e >> t;
+        if (e != N - 1 && s != N - 1 && ward[s]) continue;
+        if (e != N - 1 && s != N - 1 && ward[e]) continue;
         graph[s].push_back({ e,t });
         graph[e].push_back({ s,t });
     }
