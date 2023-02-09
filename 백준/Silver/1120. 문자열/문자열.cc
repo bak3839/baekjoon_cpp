05266d8e63e211c1514bbfb11b73724426fc013e
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-02-09 17:19:39 +0900

    [Silver IV] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int ans = 51;
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for (int i = 0; s1.size() + i <= s2.size(); i++) {
+        int cnt = 0;
+        for (int j = 0; j < s1.size(); j++) {
+            if (s1[j] != s2[j + i]) ++cnt;
+        }
+
+        ans = min(cnt, ans);
+        if (ans == 0) break;
+    }
+    
+    cout << ans;
+    return 0;
+}
