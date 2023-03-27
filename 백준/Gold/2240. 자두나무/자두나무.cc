cb0123493c598301a4b13c4ff67e59b9c56c4818
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-03-27 18:46:37 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -14,24 +14,16 @@ int main() {
 
     dp.resize(W + 2, vector<int>(T + 1, 0));
 
-    for (int i = 1;i <= T;i++) {
+    for (int i = 1; i <= T; i++) {
         cin >> tree;
-
-        if (tree == 1) {
-            for (int j = 1;j <= W + 1;j++) {
-                if (j % 2 != 0) dp[j][i] = max(dp[j][i - 1] + 1, dp[j - 1][i - 1] + 1);
-                else dp[j][i] = dp[j][i - 1];
-                ans = max(dp[j][i], ans);
-            }
+      
+        for (int j = 1; j <= W + 1; j++) {
+            if ((tree == 1 && j % 2 != 0) || (tree == 2 && j % 2 == 0)) 
+                dp[j][i] = max(dp[j][i - 1] + 1, dp[j - 1][i - 1] + 1);
+            else dp[j][i] = dp[j][i - 1];
+            ans = max(dp[j][i], ans);
         }
 
-        else {
-            for (int j = 1;j <= W + 1;j++) {
-                if (j % 2 == 0) dp[j][i] = max(dp[j][i - 1] + 1, dp[j - 1][i - 1] + 1);
-                else dp[j][i] = dp[j][i - 1];
-                ans = max(dp[j][i], ans);
-            }
-        }
     }
 
     cout << ans;
