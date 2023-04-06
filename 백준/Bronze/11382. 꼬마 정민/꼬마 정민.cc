d71ffd8eb118332f0e9413ce4420e56edf6e0eb1
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-04-06 20:57:12 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    long long a, b, c, sum = 0;
+    cin >> a >> b >> c;
+    sum = (a + b + c);
+    cout << sum;
+    return 0;
+}
