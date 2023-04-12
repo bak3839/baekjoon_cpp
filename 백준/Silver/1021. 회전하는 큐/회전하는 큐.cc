cfae2909e6549e4340b70a977b4fdc4e0c977b79
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-04-12 21:36:57 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+vector<int> want;
+deque<int> q;
+int cnt;
+
+void left(int target) {
+    while (1) {
+        if (q.front() == target) {
+            q.pop_front();
+            break;
+        }
+        else {
+            cnt++;
+            q.push_back(q.front());
+            q.pop_front();
+        }
+    }
+}
+
+void right(int target) {
+    while (1) {
+        if (q.front() == target) {
+            q.pop_front();
+            break;
+        }
+        else {
+            cnt++;
+            q.push_front(q.back());
+            q.pop_back();
+        }
+    }
+}
+
+void Solution(int N, int M) {
+    int n = M - 1;
+    while (M--) {
+        bool pass = false;
+
+        int size = q.size() / 2;
+        int target = want[n - M];
+
+        if (q.size() % 2) {
+            for (int i = size + 1; i < q.size(); i++) {
+                if (target == q[i]) pass = true;
+            }
+            if (pass) right(target);
+            else left(target);
+        }
+
+        else {
+            for (int i = size; i < q.size(); i++) {
+                if (target == q[i]) pass = true;
+            }
+            if (pass) right(target);
+            else left(target);
+        } 
+    }
+    cout << cnt;
+}
+
+void Input() {
+    int N, M;
+    cin >> N >> M;
+    want.resize(M);
+
+    for (int i = 1; i <= N; i++) q.push_back(i);
+    for (int i = 0; i < M; i++) cin >> want[i];
+
+    Solution(N, M);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    Input();
+
+    return 0;
+}
