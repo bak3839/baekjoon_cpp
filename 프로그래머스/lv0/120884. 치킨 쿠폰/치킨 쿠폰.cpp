95100eaeffe617045d5bcb49260ca5b3e1767e38
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-05-31 19:53:14 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -6,17 +6,9 @@ using namespace std;
 int solution(int chicken) {
     int rest = 0, ans = 0;
     
-    while(chicken >= 10){        
-        rest += chicken % 10;
-        chicken /= 10;
-        ans += chicken;
-    }
-    
-    rest += chicken;
-    
-    while(rest >= 10) {
-        ans += rest / 10;
-        rest = rest / 10 + rest % 10;
+    while(chicken >= 10){                
+        ans += chicken / 10;
+        chicken = chicken /10 + chicken % 10;
     }
     
     return ans;
