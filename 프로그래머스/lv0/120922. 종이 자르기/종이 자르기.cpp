69d6e179693a374451baa1559bfb885a2e6efcc7
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-06-11 20:46:46 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.cpp
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.cpp
@@ -4,11 +4,5 @@
 using namespace std;
 
 int solution(int M, int N) {
-    if(N > M) {
-        int tmp = N;
-        N = M;
-        M = tmp;
-    }
-    
-    return M * (N - 1) + (M - 1);
+    return M * N - 1;
 }
