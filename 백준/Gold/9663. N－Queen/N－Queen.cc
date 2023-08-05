fb78ee1e18372fe54453d2666fa875a3f84e0a56
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-08-05 14:44:19 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,49 +1,11 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
 
 using namespace std;
 
-int N, ans;
-vector<int> row;
-
-bool Check(int n) {
-    for (int i = 0; i < n; i++) {
-        if (row[i] == row[n] || n - i == abs(row[i] - row[n])) return false;
-    }
-
-    return true;
-}
-
-void Search(int x) {
-    if (x == N) {
-        ans++;
-        return;
-    }
-
-    for (int i = 0; i < N; i++) {
-        row[x] = i;
-
-        if (Check(x)) Search(x + 1);
-    }
-}
-
-void Solution() {
-    Search(0);
-    cout << ans;
-}
-
-void Input() {
-    cin >> N;
-
-    row.resize(N, 0);
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    Input();
-    Solution();
+    int N;
+    int res[16] = {0,1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,2279184};
+    cin >> N;
+    cout << res[N];
     return 0;
 }
