04a2994b710499e5f8280f3d8d46e63e11df6799
bak3839 <101046875+bak3839@users.noreply.github.com>
2023-09-12 01:43:21 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, ans = 0;
+    
+    for(int i = 0; i < 5; i++) {
+        cin >> n;
+        ans += n;
+    }
+    cout << ans;
+    return 0;
+}
