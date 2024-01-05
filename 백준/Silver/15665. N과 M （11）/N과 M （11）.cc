a10a80af73de8fe8e8fa8e1741e379fd5a4e8090
bak3839 <101046875+bak3839@users.noreply.github.com>
2024-01-05 15:46:45 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<bool> check;
+vector<int> num;
+vector<int> S;
+
+void Sequence() {
+    if (S.size() == M) {
+        for (int i = 0; i < M; i++)
+            cout << S[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int prev = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (num[i] == prev) continue;
+
+        prev = num[i];
+
+        S.push_back(num[i]);
+        check[i] = true;
+
+        Sequence();
+
+        S.pop_back();
+        check[i] = false;
+    }
+}
+
+void Solution() {
+    Sequence();
+}
+
+void Input() {
+    cin >> N >> M;
+
+    num.resize(N);
+    check.resize(N, false);
+
+    for (int i = 0; i < N; i++)
+        cin >> num[i];
+    sort(num.begin(), num.end());
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    Input();
+    Solution();
+    return 0;
+}
