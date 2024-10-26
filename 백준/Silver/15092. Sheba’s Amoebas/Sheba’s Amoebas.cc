4d0a70af624fd90e44197679096f6dce198c0a58
bak3839 <101046875+bak3839@users.noreply.github.com>
2024-10-26 14:20:16 +0900

    [Silver II] Title: Sheba’s Amoebas, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/15092. Sheba’s Amoebas/Sheba’s Amoebas.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+using pii = pair<int, int>;
+
+int N, M;
+int mv[8][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1} };
+vector<string> map;
+
+void Bfs(int a, int b) {
+    queue<pii> q;
+    q.push({ a, b });
+    map[a][b] = '.';
+
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + mv[i][0];
+            int ny = y + mv[i][1];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] == '.') continue;
+
+            q.push({ nx, ny });
+            map[nx][ny] = '.';
+        }
+    }
+}
+
+void Solution() {
+    int ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] != '.') {
+                Bfs(i, j);
+                ans++;
+            }
+        }
+    }
+
+    cout << ans;
+}
+
+void Input() {
+    cin >> N >> M;
+
+    map.resize(N);
+
+    for (int i = 0; i < N; i++)
+        cin >> map[i];
+}
+
+void Solve() {
+    Input();
+    Solution();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Solve();
+    return 0;
+}
