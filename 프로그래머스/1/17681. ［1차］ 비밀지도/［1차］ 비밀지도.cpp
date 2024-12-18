018ba516f1377b4ba6962ba4c153d9e9b74a17cd
bak3839 <101046875+bak3839@users.noreply.github.com>
2024-12-18 23:42:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    int mod, res;
+    
+    for(int i = 0; i < n; i++) {
+        string s1 = "", sum = "";
+        mod, res = arr1[i];
+        
+        for(int j = 0; j < n; j++) {
+            mod = res % 2;
+            res /= 2;
+            
+            if(mod == 1) {
+                s1 = "#" + s1;
+            } else {
+                s1 = " " + s1;
+            }
+        }
+        
+        res = arr2[i];
+        
+        for(int j = 0; j < n; j++) {
+            mod = res % 2;
+            res /= 2;
+            
+            if(mod == 1 || s1[n - j - 1] == '#') {
+                sum = "#" + sum;
+            } else {
+                sum = " " + sum;
+            }
+        }
+        
+        answer.push_back(sum);
+    }
+    return answer;
+}
