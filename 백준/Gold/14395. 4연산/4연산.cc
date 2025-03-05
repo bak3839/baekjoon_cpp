1df09e9acd7b73ac8d2151e48ec74a8f5ddb9f14
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-03-05 23:56:43 +0900

    [Gold IV] Title: 4연산, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/14395. 4연산/4연산.cc
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#define INF 10000000000000
+using namespace std;
+
+int s, t;
+string result;
+bool flag = false;
+
+void input(){
+    cin >> s >> t;
+}
+
+void solve(){
+    unordered_set<long long> hashmap;
+    queue<pair<long long, string>> q;
+    q.push({s, ""});
+
+    while(!q.empty()){
+        long long cur = q.front().first;
+        string str = q.front().second;
+        q.pop();
+
+        if(cur == t){
+            flag = true;
+            result = str;
+            return;
+        }
+        long long next = cur * cur;
+        if(hashmap.find(next) == hashmap.end()){
+            hashmap.insert(next);
+            q.push({cur * cur, str + "*"});
+        }
+        next = cur + cur;
+        if(hashmap.find(next) == hashmap.end()){
+            hashmap.insert(next);
+            q.push({cur + cur, str + "+"});
+        }
+        next = cur - cur;
+        if(hashmap.find(next) == hashmap.end()){
+            hashmap.insert(next);
+            q.push({cur - cur, str + "-"});
+        }
+
+        if(cur != 0){
+            next = cur / cur;
+            if(hashmap.find(next) == hashmap.end()){
+                hashmap.insert(next);
+                q.push({cur / cur, str + "/"});
+            }
+        }
+
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+    if(s == t){
+        cout << "0";
+    }
+    else{
+        solve();
+        if(flag) cout << result;
+        else cout << "-1";
+    }
+
+    return 0;
+}
