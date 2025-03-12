2a25b7632089f1329597ec3947b8a6df26749824
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-03-13 00:01:44 +0900

    [Bronze I] Title: 나무 조각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int p[6];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+	for (int i = 1; i <= 5; i++) cin >> p[i];
+
+	for (int i = 5; i >= 1; i--) {
+		for (int j = 1; j < i; j++) {
+			if (p[j] > p[j+1]) {
+				swap(p[j], p[j+1]);
+                
+				for (int k = 1; k <= 5; k++)
+					cout << p[k] << " ";
+                
+				cout << "\n";
+			}
+		}
+	}
+    
+    return 0;
+}
