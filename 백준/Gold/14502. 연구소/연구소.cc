3ff083f7b82d25bc296b7872a37879dbd8118584
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-03-17 22:32:12 +0900

    [Gold IV] Title: 연구소, Time: 48 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,99 +1,80 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
+#define map bak
 
 using namespace std;
+using pii = pair<int, int>;
 
-int N, M, safe, res, cnt_wall;
-int copy_map[8][8];
+int N, M, ans;
 int map[8][8];
-vector<pair<int, int>> virus;
+int mv[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+bool visited[8][8];
+vector<pii> wall, virus;
 
-int m1[4] = { -1, 1, 0, 0 };
-int m2[4] = { 0, 0, -1, 1 };
+int Spread() {
+    int x, y, nx, ny, cnt = 0;
+    
+    queue<pii> q;
 
-void map_cpy(int a[][8], int b[][8]) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			a[i][j] = b[i][j];
-		}
-	}
+    for (int i = 0; i < virus.size(); i++) {
+        x = virus[i].first, y = virus[i].second;
+        q.push({ x, y });
+        visited[x][y] = true;
+    }
+
+    while (q.size()) {
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            nx = x + mv[i][0];
+            ny = y + mv[i][1];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+            if (visited[nx][ny] || map[nx][ny] != 0) continue;
+
+            cnt++;
+            q.push({ nx, ny });
+            visited[nx][ny] = true;
+        }
+    }
+
+    return (wall.size() - cnt - 3);
 }
 
-void bfs() {
-	queue<pair<int, int>> q;
-	int cnt = 0;
-	int tmp[8][8];
-	map_cpy(tmp, copy_map);
+void Solve(int s, int cnt) {
+    if (cnt == 3) {
+        ans = max(ans, Spread());
+        memset(visited, false, sizeof(visited));
+        return;
+    }
 
-	for (int i = 0; i < virus.size(); i++) q.push(virus[i]);
+    for (int i = s; i < wall.size(); i++) {
+        int x = wall[i].first, y = wall[i].second;
 
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + m1[i];
-			int ny = y + m2[i];
-
-			if ((0 <= nx && nx < N) && (0 <= ny && ny < M)) {
-				if (tmp[nx][ny] == 0) {
-					tmp[nx][ny] = 2;
-					++cnt;
-					q.push(make_pair(nx, ny));
-				}
-			}
-		}
-	}
-
-	res = max(res, (safe - 3 - cnt));
-}
-
-void wall(int cnt) {
-	if (cnt == 3) {
-		bfs();
-		return;
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (copy_map[i][j] == 0) {
-				copy_map[i][j] = 1;
-				wall(cnt + 1);
-				copy_map[i][j] = 0;
-			}
-		}
-	}
+        map[x][y] = 1;
+        Solve(i + 1, cnt + 1);
+        map[x][y] = 0;
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int n;
-	cin >> N >> M;
+    cin >> N >> M;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 0) ++safe;
-			else if (map[i][j] == 2) virus.push_back(make_pair(i, j));
-		}
-	}
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> map[i][j];
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (map[i][j] == 0) {
-				map_cpy(copy_map, map);
-				copy_map[i][j] = 1;
-				wall(1);
-				copy_map[i][j] = 0;
-			}
-		}
-	}
-
-	cout << res;
-	return 0;
+            if (map[i][j] == 0) wall.push_back({ i, j });
+            else if (map[i][j] == 2) virus.push_back({ i, j });
+        }
+    }
+    
+    Solve(0, 0);
+    cout << ans;
+    return 0;
 }
