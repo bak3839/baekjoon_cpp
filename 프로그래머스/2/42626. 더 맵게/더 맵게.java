a989336ae031efa51ec73a1287ff272fb5b1bd66
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-04-18 22:20:21 +0900

    [level 2] Title: 더 맵게, Time: 1673.66 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        boolean pass = false;
+        int answer = 0;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        Arrays.stream(scoville).forEach(v -> pq.add(Long.valueOf(v)));
+        
+        while(!pq.isEmpty()) {
+            if(pq.peek() >= K) {
+                pass = true;
+                break;
+            }
+
+            Long s1 = pq.poll();
+            Long s2 = pq.poll();
+            
+            if(s2 == null) break;
+            
+            pq.add(s1 + s2 * 2);
+            
+            answer++;
+        }
+        
+        if(!pass) answer = -1; 
+        return answer;
+    }
+}
