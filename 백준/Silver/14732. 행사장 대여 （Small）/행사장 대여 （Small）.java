163503098e09f6150bf4e25e6bf14622a3645fe8
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-08-13 14:49:13 +0900

    [Silver V] Title: 행사장 대여 (Small), Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Silver/14732. 행사장 대여 （Small）/행사장 대여 （Small）.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        boolean[][] visited = new boolean[501][501];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    if (visited[x][y]) continue;
+
+                    visited[x][y] = true;
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
