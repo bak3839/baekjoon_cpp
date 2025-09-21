8834269df5f5604d4617d6c8a39294f97d5a94ee
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-09-21 22:20:57 +0900

    [Silver V] Title: 원, 탁!, Time: 424 ms, Memory: 91216 KB -BaekjoonHub

---
+++ 백준/Silver/28136. 원， 탁！/원， 탁！.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int ans = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (A[i] >= A[(i + 1) % N]) ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
