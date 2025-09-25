09a15cda6e9c5ae4e6bad719c36ebd4bde1e413a
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-09-25 21:07:39 +0900

    [Silver I] Title: 행렬, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] A, B;
+
+    public static void flip(int x, int y) {
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                A[i][j] = A[i][j] == 0 ? 1 : 0;
+            }
+        }
+    }
+
+    public static boolean compare() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (A[i][j] != B[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new int[N][M]; B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                A[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                B[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= N - 3; i++) {
+            for (int j = 0; j <= M - 3; j++) {
+                if (A[i][j] != B[i][j]) {
+                    flip(i, j);
+                    ans++;
+                }
+            }
+        }
+
+        System.out.println(compare() ? ans : -1);
+    }
+}
