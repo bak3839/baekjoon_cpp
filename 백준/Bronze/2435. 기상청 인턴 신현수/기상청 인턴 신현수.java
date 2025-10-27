bf971899fa5c0ffbb63c329c6450305b289940e4
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-10-27 23:42:31 +0900

    [Bronze I] Title: 기상청 인턴 신현수, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2435. 기상청 인턴 신현수/기상청 인턴 신현수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int ans = Integer.MIN_VALUE;
+        int[] A = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken()) + A[i - 1];
+        }
+
+        for (int i = K; i <= N; i++) {
+            ans = Math.max(ans, A[i] - A[i - K]);
+        }
+        System.out.println(ans);
+    }
+}
