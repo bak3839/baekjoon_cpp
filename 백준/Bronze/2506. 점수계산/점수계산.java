f56d6e4ee7b2dd61d16b415ed9fd1f98060b19a2
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-11-07 22:27:17 +0900

    [Bronze III] Title: 점수계산, Time: 100 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int ans = 0;
+        int point = 1;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0) {
+                point = 1;
+            } else {
+                ans += point;
+                point++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
