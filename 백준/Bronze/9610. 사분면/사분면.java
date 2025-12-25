e3ac1c07a0073277328b1f2967a3b93542a8695b
bak3839 <101046875+bak3839@users.noreply.github.com>
2025-12-25 16:51:03 +0900

    [Bronze III] Title: 사분면, Time: 128 ms, Memory: 14716 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int Q1 = 0, Q2 = 0, Q3 = 0, Q4 = 0, AXIS = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x == 0 || y == 0) AXIS++;
+            else if (x > 0 && y > 0) Q1++;
+            else if (x < 0 && y > 0) Q2++;
+            else if (x < 0 && y < 0) Q3++;
+            else Q4++;
+        }
+
+        System.out.println("Q1: " + Q1);
+        System.out.println("Q2: " + Q2);
+        System.out.println("Q3: " + Q3);
+        System.out.println("Q4: " + Q4);
+        System.out.println("AXIS: " + AXIS);
+    }
+}
