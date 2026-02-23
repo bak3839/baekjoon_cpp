83d857d7de359f017537eb740250894dd5bacc0f
bak3839 <101046875+bak3839@users.noreply.github.com>
2026-02-23 23:38:46 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("  ___  ___  ___\n" +
+            "  | |__| |__| |\n" +
+            "  |           |\n" +
+            "   \\_________/\n" +
+            "    \\_______/\n" +
+            "     |     |\n" +
+            "     |     |\n" +
+            "     |     |\n" +
+            "     |     |\n" +
+            "     |_____|\n" +
+            "  __/       \\__\n" +
+            " /             \\\n" +
+            "/_______________\\");
+    }
+}
