2eaa09419fccdbf11377bc0412be9361a3baa371
bak3839 <101046875+bak3839@users.noreply.github.com>
2026-03-19 23:51:27 +0900

    [Bronze IV] Title: MBTI, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/25640. MBTI/MBTI.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String mbti = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+        
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            if (input.equals(mbti)) {
+                ans++;
+            }
+        }
+        
+        System.out.println(ans);
+    }
+}
