5d5ed0f2cc36c0f06d24d01fa15ed870b23cac16
bak3839 <101046875+bak3839@users.noreply.github.com>
2026-04-05 21:50:22 +0900

    [Bronze III] Title: 쿠폰, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/10179. 쿠폰/쿠폰.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            double price = Double.parseDouble(br.readLine());
+            System.out.printf("$%.2f%n", price * 0.8);
+        }
+    }
+}
