d824ba2a4135f8a8a0c5071d11756e2f848bf0b2
bak3839 <101046875+bak3839@users.noreply.github.com>
2026-04-12 23:48:10 +0900

    [Bronze III] Title: 경기 결과, Time: 528 ms, Memory: 206876 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5523. 경기 결과/경기 결과.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N  = Integer.parseInt(br.readLine());
+        int A = 0, B = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a > b) A++;
+            else if (b > a) B++;
+        }
+
+        System.out.println(A + " " + B);
+    }
+}
